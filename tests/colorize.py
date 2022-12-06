36a8bd4b77d33748dcd947ba3f133a28e87b4473
Claudio Klingler <ck@realtime-projects.com>
2022-12-06 18:30:39 +0100

    remove test failure.

--- tests/colorize.py
+++ tests/colorize.py
@@ -10,4 +10,3 @@ def test_indent():
     assert re.search(r"\x1b\[30m.*This message disappears with the next log message\x1b\[0m\x1b\[1A", result.stdout)
     assert re.search(r"\x1b\[1m\x1b\[30m.*Another message.*\x1b\[0m\x1b\[1A", result.stdout)
     assert re.search(r"\x1b\[2K\x1b\[32m.*Last message is gone*\x1b\[0m", result.stdout)
-    assert False
